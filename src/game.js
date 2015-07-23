0f5dc079da85028768047e00017137410d19871c
Richard Wossal <richard@r-wos.org>
2015-07-23 02:27:00 +0200

    some more template code

--- src/game.js
+++ src/game.js
@@ -1,1 +1,48 @@
-// hello
+var MAPW = 200;
+var MAPH = 200;
+var MAPFILL = 10;
+
+var ENEMIES = [];
+var MAP = [];
+
+/// utils
+
+function rand(a, b) { return Math.floor(Math.random() * (b - a)) + a; }
+function randommappoint() { return [rand(0, MAPW), rand(0, MAPH)]; }
+
+/// main
+
+function game_init() {
+    ENEMIES = [];
+    MAP = [];
+
+    var i, p;
+
+    for (i = 0; i < MAPW; i++) {
+        MAP.push(new Array(MAPH));
+    }
+    for (i = 0; i < MAPW*MAPH / MAPFILL; i++) {
+        p = randommappoint();
+        MAP[p[0]][p[1]] = 'X';
+    }
+}
+
+function game_mapdebug(c, W, H) {
+    c.fillStyle = 'green';
+    var pw = W/MAPW;
+    var ph = H/MAPH;
+    for (var x = 0; x < MAPW; x++) {
+    for (var y = 0; y < MAPW; y++) {
+        if (MAP[x][y] === 'X') {
+            c.fillRect(x*pw, y*ph, pw, ph);
+        }
+    }}
+}
+
+function game_renderhighscores(el) {
+    // XXX TODO
+}
+
+function game_render(c, W, H) {
+    game_mapdebug(c, W, H);
+}
--- src/main.js
+++ src/main.js
@@ -1,37 +1,35 @@
-window.requestAnimFrame = (
-    function(callback) {
-      return window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function(callback) {
-            window.setTimeout(callback, 1000 / 30);
-          };
-    })();
+window.requestAnimFrame = (function(callback) {
+    return window.requestAnimationFrame ||
+           window.webkitRequestAnimationFrame ||
+           window.mozRequestAnimationFrame ||
+           window.oRequestAnimationFrame ||
+           window.msRequestAnimationFrame ||
+           function(callback) {
+               console.log("ANIMATION FRAME FALLBACK"); 
+               window.setTimeout(callback, 1000 / 30);
+           };
+})();
 
-/*** XXX
-function animate() {
-  // draw
-  var canvas = document.getElementById('canvas');
-
-  render(canvas.getContext('2d'));
-  // request new frame
-  requestAnimFrame(function() {
-    animate();
-    // Send a message if you've moved.
-    if (mouseDirty) {
-      gapi.hangout.data.sendMessage(
-          JSON.stringify([0,
-                          currentColor,
-                          lastTileX,
-                          lastTileY]));
-      mouseDirty = false;
-    }
-  });
+function mainloop() {
+    var canvas = document.getElementById('main');
+    canvas.width = canvas.scrollWidth;
+    canvas.height = canvas.scrollHeight;
+    game_render(canvas.getContext('2d'), canvas.width, canvas.height);
+    requestAnimFrame(function() {
+        mainloop();
+        /** XXX
+        // Send a message if you've moved.
+        if (mouseDirty) {
+          gapi.hangout.data.sendMessage(
+              JSON.stringify([0,
+                              currentColor,
+                              lastTileX,
+                              lastTileY]));
+          mouseDirty = false;
+        }
+        */
+    });
 }
-*/
-
 
 /** Get a message.
  * @param {MessageReceievedEvent} event An event.
@@ -66,14 +64,41 @@ function init() {
                     ev.clientY - canvas.offsetTop);
         };
         */
-        /// XXX main loop animate();
+        game_init();
+        mainloop();
 
       } catch (e) {
-        console.log('init:ERROR');
+        console.log('INIT ERROR');
         console.log(e);
       }
     }
   });
+}
+
+if (typeof gadgets === 'undefined') {
+    // shimshimshim
+    console.log("LOCAL VERSION");
+    gapi = {
+        hangout: {
+            onApiReady: {
+                add: function(fn) { fn({isApiReady: true}); }
+            },
+            layout: {
+                getVideoCanvas: function() {
+                    return {
+                        setWidth: function() {},
+                        setPosition: function() {},
+                        setVisible: function() {},
+                    }
+                }
+            }
+        }
+    };
+    gadgets = {
+        util: {
+            registerOnLoadHandler: function(fn) { fn(); }
+        }
+    };
 }
 
 gadgets.util.registerOnLoadHandler(init);
